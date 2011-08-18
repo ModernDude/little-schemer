b5dac47d8f8e49438604ad5cbbf3c63e7888b2af
Jeff Sigmon <jeff@jeffsigmon.com>
2011-08-17 20:46:19 -0500

    completed chapter 2

--- 00-common.scm
+++ 00-common.scm
@@ -5,3 +5,18 @@
     (and (not (pair? x)) (not (null? x)))))
 
 
+(define lat?
+  (lambda (l)
+    (cond
+     ((null? l) #t)
+     ((atom? (car l)) (lat? (cdr l)))
+     (else #f))))
+
+
+(define member?
+  (lambda (a lat)
+    (cond
+     ((null? lat) #f)
+     (else (or (eq? (car lat) a)
+               (member? a (cdr lat)))))))
+ 
