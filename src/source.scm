0bb389fc16a16c8fcccf8f95bfbeced1da578166
Jeff Sigmon <jeff@jeffsigmon.com>
2011-09-18 00:34:38 -0500

    rest of oh mf gawd testss

--- src/source.scm
+++ src/source.scm
@@ -308,3 +308,131 @@
 (define one?
   (lambda (n)
     (=? n 1)))
+
+
+;; remove atom from s-expression
+(define rember*
+  (lambda (a l)
+    (cond
+     ((null? l) (quote ()))
+     ((atom? (car l))
+      (cond
+       ((eq? (car l) a)
+        (rember* a (cdr l)))
+       (else (cons (car l)
+                   (rember* a (cdr l))))))
+      (else (cons (rember* a (car l))
+                  (rember* a (cdr l)))))))
+
+
+;; insert the atom new to the right of the atom old anywhere in the
+;; sexp
+(define insertR*
+  (lambda (new old l)
+    (cond
+     ((null? l) (quote ()))
+     ((atom? (car l))
+      (cond
+       ((eq? (car l) old)
+        (cons old
+              (cons new
+                    (insertR* new old
+                              (cdr l)))))
+       (else (cons (car l)
+                   (insertR* new old
+                             (cdr l))))))
+     (else (cons (insertR* new old
+                           (car l))
+                 (insertR* new old
+                           (cdr l)))))))
+
+;; count the number of times that the atom a is found in the given
+;; list
+(define occur*
+  (lambda (a l)
+    (cond
+     ((null? l) 0)
+     ((atom? (car l))
+      (cond
+       ((eq? (car l) a)
+        (add1 (occur* a (cdr l))))
+       (else (occur* a (cdr l)))))
+     (else (+ (occur* a (car l))
+              (occur* a (cdr l)))))))
+
+;; substitute new for old anywhere in the sexp l
+(define subst*
+  (lambda (new old l)
+    (cond
+     ((null? l) (quote ()))
+     ((atom? (car l))
+      (cond
+       ((eq? (car l) old)
+        (cons new
+              (subst* new old (cdr l))))
+       (else (cons (car l)
+             (subst* new old
+                     (cdr l))))))
+    (else
+     (cons (subst* new old (car l))
+           (subst* new old (cdr l)))))))
+
+(define insertL*
+  (lambda (new old l)
+    (cond
+     ((null? l) (quote ()))
+     ((atom? (car l))
+      (cond
+       ((eq? (car l) old)
+        (cons new
+              (cons old
+                    (insertL* new old
+                              (cdr l)))))
+       (else (cons (car l)
+                   (insertL* new old
+                             (cdr l))))))
+     (else (cons (insertL* new old
+                           (car l))
+                 (insertL* new old
+                           (cdr l)))))))
+
+
+;; determines if a is a member of the sexp l
+(define member*
+  (lambda (a l)
+    (cond
+     ((null? l) #f)
+     ((atom? (car l))
+      (or (eq? (car l) a)
+          (member* a (cdr l))))
+     (else (or (member* a (car l))
+               (member* a (cdr l)))))))
+
+
+;; find leftmost atom in list
+(define leftmost
+  (lambda (l)
+    (cond
+     ((atom? (car l)) (car l))
+     (else (leftmost (car l))))))
+
+
+;; same number, atom or list
+(define _equal?
+  (lambda (s1 s2)
+    (cond
+     ((and (atom? s1) (atom? s2))
+      (eqan? s1 s2))
+     ((or (atom? s1) (atom? s2))
+      #f)
+     (else (eqlist? s1 s2)))))
+
+;; tests if two lists are equal
+(define eqlist?
+  (lambda (l1 l2)
+    (cond
+     ((and (null? l1) (null? l2)) #t)
+     ((or (null? l1) (null? l2)) #f)
+     (else
+      (and (_equal? (car l1) (car l2))
+           (eqlist? (cdr l1) (cdr l2)))))))
